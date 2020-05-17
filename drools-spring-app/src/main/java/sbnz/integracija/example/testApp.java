3c2aa27e2e61cb51e13f4feb05e1a1308cecc794
marko-13 <50908634+marko-13@users.noreply.github.com>
2020-05-17 12:20:01 +0200

    Added test scenarios in testApp.java and edited gitignore

--- drools-spring-app/src/main/java/sbnz/integracija/example/testApp.java
+++ drools-spring-app/src/main/java/sbnz/integracija/example/testApp.java
@@ -16,7 +16,7 @@ public class testApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*testHighIDVIndexCountry();*/
+		testHighIDVIndexCountry();
 		testClassifyPatient();
 
 	}
